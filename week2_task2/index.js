4c9718dbf78d406d0f39a9fc6db4b834d49cd6ee
Voloshin Sergei <66827799+Voloshin-Sergei@users.noreply.github.com>
2020-09-04 21:30:10 +0300

    Update index.js

--- week2_task2/index.js
+++ week2_task2/index.js
@@ -1,13 +1,13 @@
 /**
- * @param {String[]} hashtags
- * @returns {String}
- */
-module.exports = function (hashtags) {
-var result = [];
-for (var i = 0; i < hashtags.length; i++) {
-if (result.indexOf(hashtags[i].toLowerCase()) ==-1 ) {
+ * @param {String[]} hashtags
+ * @returns {String}
+ */
+module.exports = function (hashtags) {
+var result = []; // создаем массив для результирующего списка хэштегов
+for (var i = 0; i < hashtags.length; i++) {
+if (result.indexOf(hashtags[i].toLowerCase()) ==-1 ) { // приводим хэштег к нижнему регистру и сравниваем на повторение
 result.push(hashtags[i].toLowerCase());
 }
 }
-return result.join(', ');
+return result.join(', ');
 };
