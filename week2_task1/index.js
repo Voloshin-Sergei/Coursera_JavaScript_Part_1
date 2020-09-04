2d5721e18460bfd057100830212a9c9a550d52e2
Voloshin Sergei <66827799+Voloshin-Sergei@users.noreply.github.com>
2020-09-04 21:22:04 +0300

    Update index.js

--- week2_task1/index.js
+++ week2_task1/index.js
@@ -3,13 +3,13 @@
  * @returns {String[]}
  */
 module.exports = function (tweet) {
-var hashTags = tweet.split(' ');
-var result = [];
-for (var i = 0; i < hashTags.length; i++) {
+var hashTags = tweet.split(' '); // разбиваем полученную строку на массив слов
+var result = []; // переменная под найденные хэштеги
+for (var i = 0; i < hashTags.length; i++) { //цикл по хэштегам
     var allTweets = hashTags[i];
-    if (allTweets.indexOf('#') !== - 1) {
-        allTweets = allTweets.slice(1);
-        result.push(allTweets);
+    if (allTweets.indexOf('#') !== - 1) { // по первому символу проверяем является ли слово хэштегом
+        allTweets = allTweets.slice(1); // убираем знак решетки и копируем строку
+        result.push(allTweets); // сохраняем хэштег
     }
 }
 return result;
